fc28daccc69c68357000543fb92dc0ac4298fad4
Mike Lunn <mlunn01@gmail.com>
2025-09-09 12:07:49 -0400

    chore: removed boiler plate

--- src/FileExplorer/Program.cs
+++ src/FileExplorer/Program.cs
@@ -1,24 +1,10 @@
 using FileExplorer.Files;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddTransient<IFileService, FileService>();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 app.MapFileEndpoints();
-
-
-
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
